faf56cdf96d4ee440048e0459aa6bc8e0c2676af
Marco Alexi Sta Ana <alexistaana@yahoo.com.ph>
2018-05-30 14:20:06 -0700

    Added styling on searchbar

--- app.js
+++ app.js
@@ -9,78 +9,42 @@ $(document).ready(function () {
 
 	const GEOLOCATION_URL = 'http://ip-api.com/json'
 
+	//FOURSQUARE MARKER 
 	const FOOD_LOCATION_IMAGE = 'http://www.clker.com/cliparts/R/g/O/v/U/h/google-maps-marker-for-residencelamontagne-hi.png';
 
-	//GETS GEOLOCATION OF USER
-	// let geolocationData = $.getJSON(GEOLOCATION_URL);
-	// let geoTest = geolocationData.responseJSON;
-
-	// console.log(geolocationData);
-	// console.log(geoTest.lat);
+	//DECLARE MAP VARIABLE
 	let map;
 
-
+	//GETS GEOLOCATION OF USER
 	function getLocation(callback) {
 		$.getJSON(GEOLOCATION_URL, callback);
 	}
-	let lat;
-	let lon;
 
-	getLocation(InitMap);
-
-	function InitMap(data) {
-
-		console.log("hai");
-
+	//INITIALIZES MAP
+	getLocation(function (data) {
 		let options = {
 			zoom: 10,
 			center: { lat: data.lat, lng: data.lon }
 		};
 
-		lat = data.lat;
-		lon = data.lon;
-
-		console.log(lat);
-
 		//DECLARE AND INITIALIZE MAP VARIABLE
-	 	map = new google.maps.Map(document.getElementById('map'), options);
-
-	}
-
-	
-
-	// function getDataFromMeetUp(search, callback) {
-	// 	const query = {
-	// 		text: `${search}`,
-	// 		page: '7'	
-	// 	}
-
-	// 	const settings = {
-	// 		url: GEOLOCATION_URL,
-	// 		data: query,
-	// 		dataType: 'jsonp',
-	// 		type: 'GET',
-	// 		success: callback
-	// 	}
-
-	// 	$.ajax(settings);
-	// }
-
+		map = new google.maps.Map(document.getElementById('map'), options);
+	})
 
 	//GETS THE DATA FROM MEETUP API
 	function getDataFromMeetUp(search, callback) {
-		// GetGeolocation(data);
 
+		//CALLS GETLOCATION TO RETRIEVE LOCATION OF USER
 		getLocation(function (data) {
 			const query = {
-				key: `${MEETUP_API_KEY}`,
+				key: MEETUP_API_KEY,
 				sign: "true",
 				text: `${search}`,
-				page: '7',
+				page: '8',
 				lat: `${data.lat}`,
 				lon: `${data.lon}`
 			}
-	
+
 			const settings = {
 				url: MEETUP_SEARCH_URL,
 				data: query,
@@ -88,7 +52,7 @@ $(document).ready(function () {
 				type: 'GET',
 				success: callback
 			}
-	
+
 			$.ajax(settings);
 		})
 	}
@@ -99,7 +63,7 @@ $(document).ready(function () {
 			client_id: FOURSQUARE_APP_ID,
 			client_secret: FOURSQUARE_API_SECRET,
 			ll: `${search}`,
-			categoryId: '56aa371be4b08b9a8d57350b,4bf58dd8d48988d16c941735,52e81612bcbc57f1066b7a0c,4bf58dd8d48988d16d941735,52e81612bcbc57f1066b7a00,4bf58dd8d48988d1d0941735,4bf58dd8d48988d147941735',
+			categoryId: '56aa371be4b08b9a8d57350b,4bf58dd8d48988d16c941735,52e81612bcbc57f1066b7a0c,4bf58dd8d48988d16d941735,52e81612bcbc57f1066b7a00,4bf58dd8d48988d1d0941735,4bf58dd8d48988d147941735,4bf58dd8d48988d148941735,4bf58dd8d48988d16e941735,4bf58dd8d48988d112941735,4bf58dd8d48988d1bf941735',
 			v: '20180420',
 			radius: '15000'
 		}
@@ -129,13 +93,11 @@ $(document).ready(function () {
 	//GETS THE DATA USED FOR THE MARKERS
 	function getDataApi(data) {
 
-		console.log(data);
-
 		const results = data.data.events;
 		let idx = 0;
 
 		//WHILE LOOP THAT GOES THROUGH ARRAY
-		while (idx < 5) {
+		while (idx < 6) {
 			if (results[idx]) {
 				if (typeof results[idx].venue !== "undefined") {
 					//CALLS FXN TO GET FOURSQUARE DATA
@@ -145,10 +107,8 @@ $(document).ready(function () {
 			idx++;
 		}
 
-		//INITIALIZES
+		//INITIALIZES MEETUP MARKER
 		meetupMapOptions(results);
-
-		// console.log(results)
 	}
 
 	//INITIALIZES THE MARKERS FROM FOURSQUARE
@@ -174,7 +134,7 @@ $(document).ready(function () {
 
 			let infoWindowFourSquare = new google.maps.InfoWindow({
 				content: `<h1 style="margin-bottom:5px;">${data.response.venues[idx].name}</h1>
-					  <h2 style="margin-top:5px;">${data.response.venues[idx].location.address}, ${data.response.venues[idx].location.city}</h2>`
+					  <h2 style="margin-top:5px;">${data.response.venues[idx].location.address}, <br> ${data.response.venues[idx].location.city}, ${data.response.venues[idx].location.state} </h2>`
 
 			});
 
@@ -189,12 +149,11 @@ $(document).ready(function () {
 
 	//INITIALIZES THE MARKERS FROM MEETUP
 	function meetupMapOptions(result) {
-		// console.log(result.venue);
 
 		let idx = 0;
 
 		//WHILE LOOP GOES THROUGH ARRAY
-		while (idx < 5) {
+		while (idx < 6) {
 			if (result[idx]) {
 				if (typeof result[idx].venue !== "undefined") {
 					//ADDS MARKER
@@ -206,7 +165,7 @@ $(document).ready(function () {
 
 					let infoWindow = new google.maps.InfoWindow({
 						content: `<h1 style="margin-bottom:5px;">${result[idx].name}</h1>
-								<h2 style="margin-bottom:5px; margin-top:5px;">${result[idx].venue.address_1}</h2>
+								<h2 style="margin-bottom:5px; margin-top:5px;">${result[idx].venue.address_1}, <br> ${result[idx].venue.city}, ${result[idx].venue.state}</h2>
 								<h2 style="margin-top:5px;"><a href="${result[idx].link}" target="_blank">
 								MEETUP PAGE</a></h2>`
 					});
@@ -222,5 +181,4 @@ $(document).ready(function () {
 
 
 	$(watchSubmit);
-
 });
